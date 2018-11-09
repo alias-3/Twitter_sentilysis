3ec672775b2fd7cd7d349af1ce55b3d1a9e53a75
alias-3 <wanianurag3@gmail.com>
2018-11-09 20:12:08 +0530

    csv download feature

--- sentilysis/twitter_script.py
+++ sentilysis/twitter_script.py
@@ -1,25 +1,40 @@
 import tweepy
 from textblob import TextBlob
 from . import authenticate
+import re
+
+
+def clean_tweet(tweet):
+
+    return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
 
 
 def tweets(query,cnt=20):
 	p = query
-	tweet_list = authenticate.api.search(q=p+" -RT",count=cnt)
+	tweet_list = authenticate.api.search(q=p+" -RT",count=cnt,lang='en')
 	return tweet_list
+
+
 def analysis(tweet_list):
 	positive=negative=neutral=0
-	pos_dict=neg_dict=net_dict={}
+	csv_list=[]
+	polarity_dict ={}
 	for t in tweet_list:
 		analyse = TextBlob(t.text)
 		j = analyse.sentiment.polarity
 		if j > 0:
 			positive +=1 
-
+			ex=[clean_tweet(t.text),'1']
+			csv_list.append(ex)
+			polarity_dict.update({t.text:1})
 		elif j==0:
 			neutral +=1
-
+			ex=[clean_tweet(t.text),'0']
+			csv_list.append(ex)
+			polarity_dict.update({t.text:0})
 		else:
 			negative +=1
-
-	return positive,negative,neutral
+			ex=[clean_tweet(t.text),'-1']
+			csv_list.append(ex)
+			polarity_dict.update({t.text:-1})
+	return positive,negative,neutral,csv_list,polarity_dict
--- sentilysis/views.py
+++ sentilysis/views.py
@@ -1,22 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from . import  twitter_script
-
+import json
 
 def index(request):
 	if request.method=="POST" and request.POST.get('query') != "":
 		tweet_list = twitter_script.tweets(request.POST.get('query'),request.POST.get('numtweets'))
-		pos,neg,net = twitter_script.analysis(tweet_list)
+		pos,neg,net,csv_list,pol_dict = twitter_script.analysis(tweet_list)
 		context = {
 			'tweet_list' : tweet_list,
 			'pos': (pos*100/int(request.POST.get('numtweets'))),
 			'neg': (neg*100/int(request.POST.get('numtweets'))),
 			'net': (net*100/int(request.POST.get('numtweets'))),
+			'csv_list' : json.dumps(csv_list),   
+			'csv_len' : len(csv_list),
+			'pol_dict' : pol_dict,
 			't': len(tweet_list)
 		}
 		return render(request,"index.html",context)
 	else: 
 		context = {
-			'q': request.POST.get('query') 
+			'csv_len' : 0,
+			'q': request.POST.get('query')
 		}
 		return render(request,"index.html",context)
